c1ae2eb31fae8c7e1e64c35c4337081245068049
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2022-02-04 15:24:10 +0100

    Scrolling horizontal works

--- main.js
+++ main.js
@@ -38,13 +38,13 @@ scrollHorizontal = (e) =>
 
 // Event Listeners ##############################################################
 // LEFTBAR-TOGGLE-BTN
-document.getElementById('toggleLeftBarButton').addEventListener("click", toggleVerticalBar);
+// document.getElementById('toggleLeftBarButton').addEventListener("click", toggleVerticalBar);
 
-//RIGHTBAR-TOGGLE-BTN
-document.getElementById('toggleRightBarButton').addEventListener("click", toggleVerticalBar);
+// //RIGHTBAR-TOGGLE-BTN
+// document.getElementById('toggleRightBarButton').addEventListener("click", toggleVerticalBar);
 
-//BOTTOMBAR-TOGGLE-BTN
-document.getElementById('toggleBottomBarButton').addEventListener("click", toggleVerticalBar);
+// //BOTTOMBAR-TOGGLE-BTN
+// document.getElementById('toggleBottomBarButton').addEventListener("click", toggleVerticalBar);
 
 // TOPBAR
 document.getElementById('top-bar').addEventListener("wheel", scrollHorizontal);
