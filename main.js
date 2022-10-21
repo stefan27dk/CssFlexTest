036fbc4dabe46f6a1ddd90daf0b2a02702e2ab6c
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2022-10-21 20:43:59 +0200

    Bars state in local storage

--- main.js
+++ main.js
@@ -1,56 +1,143 @@
+loadMainBarsState();
 
 // FUNCTIONS ###################################################################
 
 // TOGGLE-VERTICAL-BARS ========================================================
-function toggleBars(e)
-{ 
+function toggleBars(e) {
     let current = e.currentTarget;
     let currentBar = document.getElementById(current.value); // The toggler buttons have assigned value for the specific bar they need to toggle ex. toggler value for topbar is value="top-bar", the toggler id is id="top-bar-toggler" than later evt. you can use the value "top-bar"+"toggler" to get the toggler, but in this case we just pass it to the next funktion to use it insteat of doing getElement
-    if(currentBar.style.display === 'none')
+    
+    if (currentBar.style.display === 'none') 
     {
         currentBar.style.display = 'block';
-        // current.style.transform += "rotate(360deg)";
-        // current.style.transform = "translateY(-50%)"; // Keep at the center after show bar
+        localStorage.setItem(currentBar.id, 'block'); // Save bar state to local storage 
     }
-    else    
+    else 
     {
         currentBar.style.display = 'none';
-        // current.style.transform = "translateY(-50%)"; // Keep at the center after show bar
-        // current.style.transform += "rotate(-180deg)";
+        localStorage.setItem(currentBar.id, 'none'); // Save bar state to local storage 
     }
 
     // changeToggleBarIcon(current.value);
-    changeToggleBarIcon(current);
+    changeTogglerIcon(current);
 }
 
- 
+
 
 // Change Togglebar Icon =======================================================
-function changeToggleBarIcon(toggler)
-{
+function changeTogglerIcon(toggler) {
     // let barToggler = document.getElementById(bar+"-toggler").children[0]; // Get the toggler 
     let barTogglerText = toggler.children[0]; // Get the toggler text, which is the first child the arrow text ">>" 
-    
-    if(barTogglerText.innerHTML== "‹‹")
-    {
-        barTogglerText.innerHTML="››";
+
+    if (barTogglerText.innerHTML == "‹‹") {
+        barTogglerText.innerHTML = "››";
     }
-    else
-    {
-        barTogglerText.innerHTML="‹‹";
+    else {
+        barTogglerText.innerHTML = "‹‹";
     }
 }
 
 
 
 
-// // SCROLLING HORIZONTALY =====================================================
-scrollHorizontal = (e) =>
-{
-    e.preventDefault(); 
+// SCROLLING HORIZONTALY =================================================================
+scrollHorizontal = (e) => {
+    e.preventDefault();
     e.currentTarget.scrollLeft += e.deltaY;
 }
 
+
+
+
+
+
+// Load Bar State from local storage  =====================================================
+function loadBarState(barId)
+{
+    let currentBar = document.getElementById(barId);
+    let currentBarLStorageState = localStorage.getItem(barId);
+    
+    if (currentBarLStorageState !== null) 
+    {
+        currentBar.style.display = currentBarLStorageState;
+
+        if(currentBarLStorageState == 'none')
+        {
+            changeTogglerIcon(document.getElementById(barId+"-toggler")); // Change toggler arrows
+        }
+    }
+}
+
+
+
+
+// LOAD ALL BARS STATE - FROM LOCAL STORAGE - On Page Load =====================================================
+function loadMainBarsState() 
+{
+    loadBarState("left-bar");
+    loadBarState("right-bar");
+    loadBarState("top-bar");
+    loadBarState("bottom-bar");
+    // // Left-bar-----------------------------
+    // let leftBar = document.getElementById('left-bar');
+    // let leftBarLStorageState = localStorage.getItem('left-bar');
+
+    // // Right-bar-----------------------------
+    // let rightBar = document.getElementById('right-bar');
+    // let rightBarLStorageState = localStorage.getItem('right-bar');
+
+    // // Top-bar-----------------------------
+    // let topBar = document.getElementById('top-bar');
+    // let topBarLStorageState = localStorage.getItem('top-bar');
+
+    // // Bottom-bar-----------------------------
+    // let bottomBar = document.getElementById('bottom-bar');
+    // let bottomBarLStorageState = localStorage.getItem('bottom-bar');
+
+
+
+
+    // // One liner probably 33% slower
+    // // leftBar.style.display = ((leftBarLStorageState !== null) ? leftBarLStorageState : "block");
+
+    // // Left-bar-----------------------------
+    // if (leftBarLStorageState !== null) 
+    // {
+    //     leftBar.style.display = leftBarLStorageState;
+    //     changeTogglerIcon(current);
+    // }
+   
+
+
+    // // Right-bar-----------------------------
+    // if (rightBarLStorageState !== null)
+    // {
+    //     rightBar.style.display = rightBarLStorageState;
+    //     changeTogglerIcon(current);
+    // }
+    
+
+
+    // // Top-bar-----------------------------
+    // if (topBarLStorageState !== null)
+    // {
+    //     topBar.style.display = topBarLStorageState;
+    //     changeTogglerIcon(current);
+    // }
+     
+
+
+
+
+    // // Bottom-bar-----------------------------
+    // if (bottomBarLStorageState !== null)
+    // {
+    //     bottomBar.style.display = bottomBarLStorageState;
+    //     changeTogglerIcon(current);
+    // }
+    
+
+}
 
 
 
@@ -59,7 +146,7 @@ scrollHorizontal = (e) =>
 // LEFTBAR-TOGGLE-BTN
 document.getElementById('left-bar-toggler').addEventListener("click", toggleBars);
 
- //RIGHTBAR-TOGGLE-BTN
+//RIGHTBAR-TOGGLE-BTN
 document.getElementById('right-bar-toggler').addEventListener("click", toggleBars);
 
 //TOPBAR-TOGGLE-BTN
@@ -70,12 +157,12 @@ document.getElementById('bottom-bar-toggler').addEventListener("click", toggleBa
 
 
 // TOPBAR
-document.getElementById('top-bar-wrapper').addEventListener("wheel", scrollHorizontal, {passive: true});
+document.getElementById('top-bar-wrapper').addEventListener("wheel", scrollHorizontal, { passive: true });
 
 // BOTTOMBAR
-document.getElementById('bottom-bar-wrapper').addEventListener("wheel", scrollHorizontal, {passive: true});
+document.getElementById('bottom-bar-wrapper').addEventListener("wheel", scrollHorizontal, { passive: true });
 
 
 
- 
+
 
