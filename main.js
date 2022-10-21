5fca13d7e51895262832e31869ab73f58fcf1bb9
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2022-10-21 20:48:51 +0200

    Cleanup

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 loadMainBarsState();
 
-// FUNCTIONS ###################################################################
+// FUNCTIONS #################################################################################################
 
-// TOGGLE-VERTICAL-BARS ========================================================
+// TOGGLE-VERTICAL-BARS ========================================================================================
 function toggleBars(e) {
     let current = e.currentTarget;
     let currentBar = document.getElementById(current.value); // The toggler buttons have assigned value for the specific bar they need to toggle ex. toggler value for topbar is value="top-bar", the toggler id is id="top-bar-toggler" than later evt. you can use the value "top-bar"+"toggler" to get the toggler, but in this case we just pass it to the next funktion to use it insteat of doing getElement
@@ -24,7 +24,7 @@ function toggleBars(e) {
 
 
 
-// Change Togglebar Icon =======================================================
+// Change Togglebar Icon ==========================================================================================
 function changeTogglerIcon(toggler) {
     // let barToggler = document.getElementById(bar+"-toggler").children[0]; // Get the toggler 
     let barTogglerText = toggler.children[0]; // Get the toggler text, which is the first child the arrow text ">>" 
@@ -40,7 +40,7 @@ function changeTogglerIcon(toggler) {
 
 
 
-// SCROLLING HORIZONTALY =================================================================
+// SCROLLING HORIZONTALY - USED IN TOP / BOTTOM BAR =================================================================
 scrollHorizontal = (e) => {
     e.preventDefault();
     e.currentTarget.scrollLeft += e.deltaY;
@@ -49,9 +49,8 @@ scrollHorizontal = (e) => {
 
 
 
-
-
-// Load Bar State from local storage  =====================================================
+// LOCAL STORAGE :: START::
+// Load Bar State from local storage  ===============================================================================
 function loadBarState(barId)
 {
     let currentBar = document.getElementById(barId);
@@ -71,78 +70,25 @@ function loadBarState(barId)
 
 
 
-// LOAD ALL BARS STATE - FROM LOCAL STORAGE - On Page Load =====================================================
+
+
+// LOAD ALL BARS STATE - FROM LOCAL STORAGE - On Page Load ==========================================================
 function loadMainBarsState() 
 {
     loadBarState("left-bar");
     loadBarState("right-bar");
     loadBarState("top-bar");
     loadBarState("bottom-bar");
-    // // Left-bar-----------------------------
-    // let leftBar = document.getElementById('left-bar');
-    // let leftBarLStorageState = localStorage.getItem('left-bar');
-
-    // // Right-bar-----------------------------
-    // let rightBar = document.getElementById('right-bar');
-    // let rightBarLStorageState = localStorage.getItem('right-bar');
-
-    // // Top-bar-----------------------------
-    // let topBar = document.getElementById('top-bar');
-    // let topBarLStorageState = localStorage.getItem('top-bar');
-
-    // // Bottom-bar-----------------------------
-    // let bottomBar = document.getElementById('bottom-bar');
-    // let bottomBarLStorageState = localStorage.getItem('bottom-bar');
-
-
-
-
-    // // One liner probably 33% slower
-    // // leftBar.style.display = ((leftBarLStorageState !== null) ? leftBarLStorageState : "block");
-
-    // // Left-bar-----------------------------
-    // if (leftBarLStorageState !== null) 
-    // {
-    //     leftBar.style.display = leftBarLStorageState;
-    //     changeTogglerIcon(current);
-    // }
-   
-
-
-    // // Right-bar-----------------------------
-    // if (rightBarLStorageState !== null)
-    // {
-    //     rightBar.style.display = rightBarLStorageState;
-    //     changeTogglerIcon(current);
-    // }
-    
-
-
-    // // Top-bar-----------------------------
-    // if (topBarLStorageState !== null)
-    // {
-    //     topBar.style.display = topBarLStorageState;
-    //     changeTogglerIcon(current);
-    // }
-     
-
-
-
-
-    // // Bottom-bar-----------------------------
-    // if (bottomBarLStorageState !== null)
-    // {
-    //     bottomBar.style.display = bottomBarLStorageState;
-    //     changeTogglerIcon(current);
-    // }
-    
-
 }
+// LOCAL STORAGE :: END::
 
 
 
 
-// Event Listeners ##############################################################
+
+
+
+// Event Listeners ####################################################################################################
 // LEFTBAR-TOGGLE-BTN
 document.getElementById('left-bar-toggler').addEventListener("click", toggleBars);
 
